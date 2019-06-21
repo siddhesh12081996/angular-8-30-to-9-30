823a13e3425c74f0efeb757e8abec7ce1b19ccc4
Siddhesh <50043728+siddhesh12081996@users.noreply.github.com>
2019-06-21 14:57:35 +0530

    Add files via upload

---
+++ oop.ts
@@ -0,0 +1,26 @@
+-    import { GpsListener } from './inter';
+import { Car } from './Car';
+    export class Car implements GpsListener {
+        private speed: number
+    
+        speedUp(): void {
+            this.speed++
+        }
+    }
+    
+        locate(lat?: number, lng?: number) { //optional parameters
+    
+        }
+    
+        turnOnIngnition(thrust: number = 10) { // default parameters
+    
+        }
+    
+    
+    let cr = new Car()
+    cr.speedUp()
+    cr.speedUp()
+    cr.locate(10, 56)
+    cr.locate()
+    cr.turnOnIngnition(59)
+    cr.turnOnIngnition()
