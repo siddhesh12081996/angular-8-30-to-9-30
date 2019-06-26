b7b74d53e24bbfab3bf9af061831b8d908bd8c29
Siddhesh <50043728+siddhesh12081996@users.noreply.github.com>
2019-06-26 16:00:34 +0530

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { H1DataComponent } from './h1-data/h1-data.component';
+import { SmallAlertComponent } from './small-alert/small-alert.component';
+import { BoldAlertComponent } from './small-alert/bold-alert.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { NvComponent } from './pricing/nv/nv.component';
+import { InfoComponent } from './pricing/info/info.component';
+import { PricingCardComponent } from './pricing/pricing-card/pricing-card.component';
+import { FooterComponent } from './pricing/footer/footer.component';
+import { FooterItemComponent } from './pricing/footer/footer-item/footer-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    H1DataComponent,
+    SmallAlertComponent,
+    BoldAlertComponent,
+    PricingComponent,
+    NvComponent,
+    InfoComponent,
+    PricingCardComponent,
+    FooterComponent,
+    FooterItemComponent
   ],
   imports: [
     BrowserModule
